bd353a5306dbf130c950910edab18b8aa9df2a0f
tiaan-jonker <tiaanj06@gmail.com>
2022-07-04 10:19:00 +1200

    MongoDB user model added

---
+++ auth/src/models/user.ts
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose
+
+// Interface describing the props required to create new User
+
+interface UserAttributes {
+  name: string
+  email: string
+  password: string
+  date: Date
+}
+
+// Interface describing the props that a User Model has
+// basically letting TS know that there is going to be a
+// build function available on user model
+
+interface UserModel extends mongoose.Model<UserDocument> {
+  build(attributes: UserAttributes): UserDocument
+}
+
+// Interface describing the props that a User Document has
+// that is a single user has
+
+interface UserDocument extends mongoose.Document {
+  name: string
+  email: string
+  password: string
+  date: Date
+}
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+// For effective type checking, cannot use new User,
+// instead call, with one extra step:
+UserSchema.statics.build = (attributes: UserAttributes) => {
+  return new User(attributes)
+}
+
+const User = mongoose.model<UserDocument, UserModel>('User', UserSchema)
+
+export { User }
