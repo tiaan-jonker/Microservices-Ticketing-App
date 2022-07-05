7dfaee01e287108363a07caea5f0bf0bb7f97e8c
tiaan-jonker <tiaanj06@gmail.com>
2022-07-05 15:00:12 +1200

    Current user and signout routes added

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 const router = express.Router()
 
 // @route   GET api/v1/users/currentuser
@@ -7,9 +8,25 @@ const router = express.Router()
 // @endpoint ticketing.dev/api/v1/users/currentuser
 
 // current user steps
+// 1. currUser req either has jwt with cookie or not
+// 2. check req.session.jwt if it exists
+// 3. not set or invalid, return early (currentUser: null)
+// 4. set and jwt valid, send back info stored inside JWT (payload)
 
-router.get('/currentuser', (req: any, res: any) => {
-  res.send('Working')
+// session cookie is included. If cookie is deleted the current user should be null
+
+router.get('/currentuser', (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null })
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+
+    res.send({ currentUser: payload })
+  } catch (error) {
+    res.send({ currentUser: null })
+  }
 })
 
 export { router as currentUserRouter }
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,8 +1,19 @@
-const express = require('express')
+import express, { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 const router = express.Router()
 
+// @route   POST api/v1/users/signout
+// @desc    Signin existing user
+// @access  Public
+// @endpoint ticketing.dev/api/v1/users/signout
+
+// send back a header telling users browser to empty out cookie,
+// this will remove jwt so no follow up requests are possible
+
 router.post('/signout', (req: any, res: any) => {
-  res.send('Working')
+  req.session = null
+
+  res.send({})
 })
 
 export { router as signoutRouter }
